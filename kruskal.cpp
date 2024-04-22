82bd9a360c8729d059167d71dbca0b60b31b67a1
umma Habiba <122389442+sunairakhan@users.noreply.github.com>
2024-04-22 11:01:29 +0600

    Create kruskal.cpp

---
+++ kruskal.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int,pair<int,int>>> edg;
+    for(int i = 0; i < m; i++)
+    {
+        int a, b, wt;
+        cin >> a >> b >> wt;
+        edg.push_back({wt, {a, b}});
+    }
+    sort(edg.begin(), edg.end());
+    for(auto &it : edg)
+    {
+        int wt = it.first;
+        int u = it.second.first;
+        int v = it.second.second;
+
+        cout << "Weight: " << wt << " Source: " << u << " -> " << v << endl;
+    }
+    
+    return 0;
+}
